4f3c7c9bfd5f061eb18d2b2a35a0a5c43c028d06
ezio.santori <ezio.santori@deltatre.com>
2023-11-15 00:30:29 +0100

    feat(asp.net-6): setup examples

---
+++ net6/ASP.NET 6 Features/README.md
@@ -0,0 +1,10 @@
+# ASP.NET Core MVC and Razor Pages
+- CSS isolation
+- Async Streaming
+- Bootstrap 5.1
+- Optional parameters for View Component Tag Helpers
+- 
+# Blazor new Features
+- Hot Reload
+- Web Assembly AOT
+- Hybrid apps with MAUI
---
+++ net6/console.examples/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Periodic Timer
+var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+var count=1;
+while (await timer.WaitForNextTickAsync()){
+ // do some stuff
+    Console.WriteLine(count);
+    if(count > 10)
+        break;
+    count ++;
+}
+
+// Parallel Foreach
+var urls = new[] {
+  "https://restcountries.com/v3.1/all?fields=name,flags",
+  "https://api.publicapis.org/entries"
+};
+var client = new HttpClient();
+await Parallel.ForEachAsync(urls, async (url, token) =>
+{
+  var resp = await client.GetAsync(url);
+  Console.WriteLine(resp.StatusCode);
+});
+
